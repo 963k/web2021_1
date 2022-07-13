d02ab6014f9e2b017273615fac910afd613a139e
kurumi <107976277+963k@users.noreply.github.com>
2022-07-13 05:01:16 +0000

    viwsを修正

--- app6.js
+++ app6.js
@@ -8,7 +8,7 @@ app.set('view engine', 'ejs');
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  const message = "Hello world";
+  const message = "こんにちは";
   res.render('show', {mes:message});
 });
 
@@ -23,17 +23,17 @@ app.get("/db", (req, res) => {
     })
 })
 app.get("/top", (req, res) => {
-    //console.log(req.query.pop);    // ①
+    console.log(req.query.pop);    // ①
     let desc = "";
     if( req.query.desc ) desc = " desc";
-    let sql = "select id, name, income,　maker from animefilm order by income" + desc + " limit " + req.query.pop + ";";
-    //console.log(sql);    // ②
+    let sql = "select id, name, income, maker_id from animefilm order by income" + desc + " limit " + req.query.pop + ";";
+    console.log(sql);    // ②
     db.serialize( () => {
         db.all(sql, (error, data) => {
             if( error ) {
                 res.render('show', {mes:"エラーです"});
             }
-            //console.log(data);    // ③
+            console.log(data);    // ③
             res.render('select', {data:data});
         })
     })
@@ -41,7 +41,7 @@ app.get("/top", (req, res) => {
 
 app.get("/db/:id",(req,res)=>{
   db.serialize(()=>{
-    db.all("select id,name,income,maker from animefilm where id = " + req.params.id + ";",(error,row)=>{
+    db.all("select id,name,income,maker_id from animefilm where id = " + req.params.id + ";",(error,row)=>{
       if(error){
         res.render('show',{mes:"エラーです"});
       }
